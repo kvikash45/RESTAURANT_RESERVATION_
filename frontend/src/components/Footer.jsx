ec42827eed35bca6ed72668a6a00117c6fefb3ba
Vikash Kumar <kumarvikashjnv5@gmail.com>
2024-07-02 19:49:48 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">CookBook Cafe</div>
+          <div className="right">
+            <p>Patliputra Colony, Patna, Bihar 800013</p>
+            <p>Open: 11:30 AM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By VIKASH KUMAR</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Vikash Kumar.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
